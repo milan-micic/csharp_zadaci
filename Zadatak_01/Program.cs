9c16344e1c4b74d981c6a2f5743a231da33abd45
milan-micic <milan2micic@yahoo.com>
2024-03-07 17:21:46 +0100

    Zadatak 1

---
+++ Zadatak_01/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+string? word = "";
+
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+while (word.Length != 3) {
+  Console.Write("Unesite rec od 3 slova: ");
+  word = Console.ReadLine();
+}
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+
+for (int i = 0; i < word.Length; i++) {
+  Console.Write($"{word[i]}\t");
+}
---
+++ Zadatak_01/README.md
@@ -0,0 +1,3 @@
+# Zadatak 1
+
+Sastaviti program koji unetu rec od tri slova razlaze na slova od kojih se sastoji i izdaje slova u tri polja jednog reda na ekranu.
